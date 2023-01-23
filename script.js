ae541883169d6c45469b458f2d492c2a66e26eb8
GiovanniTidei <60188364+GiovanniTidei@users.noreply.github.com>
2023-01-23 22:56:55 +0100

    Add files via upload
    add new project (didattica)

---
+++ script.js
@@ -0,0 +1,30 @@
+// Assegna la funzione showFooterOnScroll all'evento scroll della finestra
+window.addEventListener("scroll", showFooterOnScroll);
+
+var string =
+    "Sostengo allo studio in tutte le materie in presenza e/o online dagli 8 ai 16 anni";
+var str = string.split("");
+var el = document.getElementById("str");
+(function animate() {
+    str.length > 0 ? (el.innerHTML += str.shift()) : clearTimeout(running);
+    var running = setTimeout(animate, 90);
+})();
+
+// Recupera l'elemento footer nn so eprché nn funzia vabe
+var footer = document.getElementById("stickyfoot");
+
+// Funzione per mostrare il footer quando si scrolla la pagina
+function showFooterOnScroll() {
+    // Recupera la posizione attuale dello scroll
+    var currentScroll = window.pageYOffset;
+
+    // Recupera l'altezza della pagina
+    var pageHeight = document.body.offsetHeight;
+
+    // Condizione per mostrare il footer quando si è scrollato il 90% della pagina
+    if (currentScroll >= pageHeight * 0.4) {
+        footer.style.display = "block";
+    } else {
+        footer.style.display = "none";
+    }
+}
